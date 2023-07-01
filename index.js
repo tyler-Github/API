c6d189c86ceec2344e1dd88e630d86ab1c0b823b
RollViral <51767087+tyler-Github@users.noreply.github.com>
2023-07-01 16:30:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,149 +1,59 @@
-<!-- index.ejs -->
-<!DOCTYPE html>
-<html>
-<head>
-  <title>API Dashboard</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="/dashboard.css">
-</head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="/">Yasifys</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="/">Home</a>
-          </li>
-          <!-- Add additional navigation links here -->
-        </ul>
-      </div>
-    </div>
-  </nav>
+const express = require('express');
+const axios = require('axios');
+const http = require('http');
+const socketIO = require('socket.io');
 
-  <div class="container">
-    <div class="row mt-5">
-      <div class="col-lg-6 offset-lg-3">
-        <div class="card shadow-lg">
-          <div class="card-body">
-            <h4 class="card-title">API Dashboard</h4>
-            <form id="requestForm">
-              <div class="mb-3">
-                <label for="method" class="form-label">Request Type:</label>
-                <select class="form-select" id="method" name="method" required>
-                  <option class="GetRequest" value="get">GET</option>
-                  <option class="PostRequest" value="post">POST</option>
-                  <option class="PutRequest" value="put">PUT</option>
-                  <option class="DeleteRequest" value="delete">DELETE</option>
-                </select>
-              </div>
-              <div class="mb-3">
-                <label for="url" class="form-label">URL:</label>
-                <input type="text" class="form-control" id="url" name="url" placeholder="Enter URL" required>
-              </div>
-              <div id="headers-container">
-                <h6 class="mb-3">Headers:</h6>
-                <div class="input-group mb-2">
-                  <input type="text" class="form-control" name="headerKey[]" placeholder="Enter header key">
-                  <input type="text" class="form-control" name="headerValue[]" placeholder="Enter header value">
-                  <button class="btn btn-outline-secondary remove-header-btn" type="button">Remove</button>
-                </div>
-              </div>
-              <center>
-                <button class="btn btn-outline-primary add-header-btn" type="button">Add Header</button>
-                <button type="submit" class="btn btn-outline-primary add-header-btn">Send Request</button>
-              </center>
-            </form>
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const port = 3000;
 
-            <div id="responseContainer" class="mt-4" style="display: none;">
-              <h5 class="mb-3">Response:</h5>
-              <div class="request">
-                <div class="request-header">Request</div>
-                <div class="request-body">
-                  <div><strong>Method:</strong> <span id="requestMethod"></span></div>
-                  <div><strong>URL:</strong> <span id="requestURL"></span></div>
-                  <div><strong>Headers:</strong></div>
-                  <ul id="requestHeaders"></ul>
-                </div>
-                <div class="request-response">
-                  <div class="request-header">Response</div>
-                  <pre><code class="json" id="responseJSON"></code></pre>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-  <script src="https://cdn.socket.io/4.3.1/socket.io.min.js"></script>
-  <script>
-    document.addEventListener('DOMContentLoaded', () => {
-      const socket = io();
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
 
-      const requestForm = document.getElementById('requestForm');
-      const responseContainer = document.getElementById('responseContainer');
-      const requestMethodElement = document.getElementById('requestMethod');
-      const requestURLElement = document.getElementById('requestURL');
-      const requestHeadersElement = document.getElementById('requestHeaders');
-      const responseJSONElement = document.getElementById('responseJSON');
+app.get('/', (req, res) => {
+  res.render('index', { request: req, response: null });
+});
 
-      requestForm.addEventListener('submit', (event) => {
-        event.preventDefault();
+function formatJSON(obj) {
+  const jsonString = JSON.stringify(obj, null, 2);
+  const htmlString = jsonString
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"([^"]+)":/g, '<span>"$1":</span>')
+    .replace(/<span class="property">"(.*?)"<\/span>/g, '<span class="property">"$1"</span>')
+    .replace(/"([^"]+)"/g, '<span class="string">"$1"</span>')
+    .replace(/\b(true|false)\b/g, '<span class="boolean">$1</span>')
+    .replace(/\b(null)\b/g, '<span class="null">$1</span>')
+    .replace(/\b(\d+)\b/g, '<span class="number">$1</span>')
+    .replace(/([\{\}\[\],])/g, '<span class="punctuation">$1</span>');
 
-        const method = document.getElementById('method').value;
-        const url = document.getElementById('url').value;
-        const headerKeys = document.getElementsByName('headerKey[]');
-        const headerValues = document.getElementsByName('headerValue[]');
+  return htmlString;
+}
 
-        const headers = Array.from(headerKeys).map((keyElement, index) => {
-          const valueElement = headerValues[index];
-          return { key: keyElement.value, value: valueElement.value };
-        });
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
-        socket.emit('request', { method, url, headers });
-      });
+  socket.on('request', async (requestData) => {
+    try {
+      const { method, url, headers, data } = requestData;
+      const parsedHeaders = headers ? JSON.parse(headers) : {};
+      const response = await axios({ method, url, headers: parsedHeaders, data });
+      socket.emit('response', { data: response.data });
+    } catch (error) {
+      socket.emit('response', { error: error.message });
+    }
+  });
 
-      socket.on('response', (response) => {
-        responseContainer.style.display = 'block';
-        requestMethodElement.textContent = response.request.method;
-        requestURLElement.textContent = response.request.url;
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
 
-        requestHeadersElement.innerHTML = '';
-        response.request.headers.forEach((header) => {
-          const li = document.createElement('li');
-          li.textContent = `${header.key}: ${header.value}`;
-          requestHeadersElement.appendChild(li);
-        });
-
-        responseJSONElement.textContent = JSON.stringify(response.data, null, 2);
-      });
-    });
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
-  <script>
-    document.querySelector('.add-header-btn').addEventListener('click', () => {
-      const headersContainer = document.querySelector('#headers-container');
-      const inputGroup = document.createElement('div');
-      inputGroup.classList.add('input-group', 'mb-2');
-      inputGroup.innerHTML = `
-        <input type="text" class="form-control" name="headerKey[]" placeholder="Enter header key">
-        <input type="text" class="form-control" name="headerValue[]" placeholder="Enter header value">
-        <button class="btn btn-outline-secondary remove-header-btn" type="button">Remove</button>
-      `;
-      headersContainer.appendChild(inputGroup);
-
-      const removeHeaderBtns = document.querySelectorAll('.remove-header-btn');
-      removeHeaderBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-          btn.parentElement.remove();
-        });
-      });
-    });
-  </script>
-</body>
-</html>
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
